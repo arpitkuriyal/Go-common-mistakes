6b84c547823970092f871cc7fbe35a05957d89aa
Arpit Kuriyal <arpitkuriyal2002@gmail.com>
2026-01-11 13:19:45 +0530

    learning aws

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("hello world")
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
 }
